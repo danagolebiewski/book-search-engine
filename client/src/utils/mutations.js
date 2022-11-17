2c2295b99afc272a9aa36daa50cf11e385b53fd6
Dana Golebiewski <dana.golebiewski@gmail.com>
2022-11-17 13:09:05 -0700

    updated schemas, mutations, and queries

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,92 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation loginUser(
+    $email: String!, 
+    $password: String!) {
+      {
+        login(
+          email: $email
+          password: $password
+        ) {
+          token
+          user {
+            _id
+            username
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+mutation addUser(
+  $username: String!
+  $email: String!
+  $password: String!
+) {
+  addUser(
+    username: $username
+    email: $email
+    password: $password
+  ) {
+    token
+    user {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        bookId
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+}
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(
+   $newBook: InputBook!
+  ) {
+    saveBook(bookBook: $newBook) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql `
+  mutation removeBook(
+    $bookID: ID
+  ) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
